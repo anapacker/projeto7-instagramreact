273e7d22a799ddf2451bbfec19b16dde560d5f0e
Ana Packer <anaamaria.packer@gmail.com>
2022-11-18 11:20:33 -0300

    primeiro commit

---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+
+
+<header class="navbar">
+    <div class="menu">
+        <div class="logos">
+            <ion-icon name="logo-instagram"></ion-icon>
+            <div class="barra"></div>
+            <img src="logo.png" />
+        </div>
+    </div>
+</header>
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom';
+import Navbar from "./Navbar";
+import Corpo from "./Corpo";
+import Mobile from "./Mobile";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+            <Mobile />
+
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.querySelector(".root"));
